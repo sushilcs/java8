eaeb64d6349e3209e84a62a86aab4b09ce08b265
sushil kumar <sushilkr14dec@gmail.com>
2023-12-31 17:47:04 +0530

    adding missing code

--- java8practice/src/com/sk/java8/streamPractice/StreamExample.java
+++ java8practice/src/com/sk/java8/streamPractice/StreamExample.java
@@ -1,5 +1,18 @@
 package com.sk.java8.streamPractice;
 
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * find out the  sum of integers greater than 10
+ */
 public class StreamExample {
+	
+	public static void main(String args[]) {
+		List<Integer>list=Arrays.asList(11,12,13,14,15,16,17,18,19);
+		int result=list.stream().filter(i->i>10).mapToInt(i->i).sum();
+		System.out.println(result);
+	}
 
 }
+

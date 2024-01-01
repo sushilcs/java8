31523c81c560ce32a9b47ceaae6ce4742a8fc61b
sushil kumar <sushilkr14dec@gmail.com>
2024-01-01 20:27:04 +0530

    new  problems solutions

--- java8practice/src/com/sk/java8/streamPractice/EmployeeMergingAndSorting.java
+++ java8practice/src/com/sk/java8/streamPractice/EmployeeMergingAndSorting.java
@@ -12,7 +12,7 @@ public class EmployeeMergingAndSorting {
 	
 	public static class Employee{
 		private String name;
-		private int age;
+		private Long age;
 		
 		
 		public String getName() {
@@ -25,17 +25,17 @@ public class EmployeeMergingAndSorting {
 		}
 
 
-		public int getAge() {
+		public Long getAge() {
 			return age;
 		}
 
 
-		public void setAge(int age) {
+		public void setAge(Long age) {
 			this.age = age;
 		}
 
 
-		Employee(String name,int age){
+		Employee(String name,Long age){
 			this.name=name;
 			this.age=age;
 			
@@ -49,17 +49,31 @@ public class EmployeeMergingAndSorting {
 		
 	}
 	public static void main(String args[]) {
-		Employee employee1 = new Employee("Sushil",24);
-		Employee employee2 = new Employee("Ravi",21);
-		Employee employee3 = new Employee("Akash",22);
-		Employee employee4 = new Employee("Prakash",23);
-		Employee employee5 = new Employee("Rohit",25);
-		Employee employee6 = new Employee("Mohit",26);
-		List<Employee> emplist1=Arrays.asList(employee1,employee2,employee3);
-		List<Employee> emplist2=Arrays.asList(employee4,employee5,employee6);
-		List<Employee>mergedAndSortedList=Stream.concat(emplist1.stream(),emplist2.stream())
-		.sorted((e1,e2)->e1.name.compareTo(e2.name)).collect(Collectors.toList());
-		System.out.println(mergedAndSortedList);
+		Employee employee1 = new Employee("Sushil",24L);
+		Employee employee2 = new Employee("Ravi",21L);
+		Employee employee3 = new Employee("Akash",22L);
+		Employee employee4 = new Employee("Prakash",23L);
+		Employee employee5 = new Employee("Rohit",25L);
+		Employee employee6 = new Employee("Mohit",26L);
+		
+		  List<Employee> emplist1=Arrays.asList(employee1,employee2,employee3);
+		  List<Employee> emplist2=Arrays.asList(employee4,employee5,employee6);
+		/*  List<Employee>mergedAndSortedList=Stream.concat(emplist1.stream(),emplist2.
+		  stream())
+		  .sorted((e1,e2)->e1.name.compareTo(e2.name)).collect(Collectors.toList());
+		  System.out.println(mergedAndSortedList);*/
+		 
+		
+		
+		/*
+		 * Sort employee byname and salary using java 8 stream API
+		 */
+		  
+		  List<Employee>mergedAndSorted=Stream.concat(emplist1.stream(),emplist2.stream()).
+				  sorted((e1,e2)->(int)(e1.age-e2.age)).sorted((e1,e2)->e1.getName().compareTo(e2.getName()))
+		  .collect(Collectors.toList());
+		  System.out.println(mergedAndSorted);
+	
 		
 	}
 
